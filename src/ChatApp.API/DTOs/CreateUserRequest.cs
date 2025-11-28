f122a0fa63584da4de572cea265ba0f0483918cb
MarlyH <161419386+MarlyH@users.noreply.github.com>
2025-11-29 12:15:53 +1300

    Add UserController and CreateUserRequest DTO

---
+++ src/ChatApp.API/DTOs/CreateUserRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.API.DTOs
+{
+    public class CreateUserRequest
+    {
+        [Required]
+        [MaxLength(16, ErrorMessage = "Username can only be 16 characters or less.")]
+        public required string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        public required string Password { get; set; }
+    }
+}
