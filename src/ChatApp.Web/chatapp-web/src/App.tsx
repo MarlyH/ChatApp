36aa0e7b10b44e452611d0a77f7147583d3f6109
MarlyH <161419386+MarlyH@users.noreply.github.com>
2025-11-29 15:55:21 +1300

    Add NavBar component and integrate DaisyUI

--- src/ChatApp.Web/chatapp-web/src/App.tsx
+++ src/ChatApp.Web/chatapp-web/src/App.tsx
@@ -1,16 +1,20 @@
 import {Route, Routes} from "react-router";
 import Landing from "./Pages/Landing.tsx";
 import Register from "./Pages/Register.tsx";
+import NavBar from "./Components/NavBar.tsx";
 
 function App() {
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-        <Routes>
-            <Route path="/" Component={Landing}  />
-            <Route path={"/register"} Component={Register} />
-        </Routes>
-    </div>
+      <>
+          <NavBar />
+          <div className="flex flex-col items-center justify-center h-screen">
+              <Routes>
+                  <Route path="/" Component={Landing}  />
+                  <Route path={"/register"} Component={Register} />
+              </Routes>
+          </div>
+      </>
   )
 }
 
