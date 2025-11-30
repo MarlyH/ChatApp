b4d35b9a0992808acd7250f62ea3eda4a58f9b4f
MarlyH <161419386+MarlyH@users.noreply.github.com>
2025-11-30 15:19:28 +1300

    Add generic ServiceResult DTO
    - Introduce a generic class to standardize service layer responses

---
+++ src/ChatApp.API/DTOs/ServiceResult.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatApp.API.DTOs
+{
+    /// <summary>
+    /// Generic service result DTO
+    /// </summary>
+    public class ServiceResult<T>
+    {
+        public bool Succeeded { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
