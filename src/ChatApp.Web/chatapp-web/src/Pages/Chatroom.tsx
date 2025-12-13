96a73d5ad0c12f17cbfcb5912c301af6319234ed
MarlyH <161419386+MarlyH@users.noreply.github.com>
2025-12-13 15:57:12 +1300

    Add room details fetching to Chatroom page

--- src/ChatApp.Web/chatapp-web/src/Pages/Chatroom.tsx
+++ src/ChatApp.Web/chatapp-web/src/Pages/Chatroom.tsx
@@ -1,12 +1,56 @@
 import {useParams} from "react-router";
+import {useCallback, useEffect, useState} from "react";
+
+interface GetRoomDetailsResponse {
+    name: string;
+}
 
 export default function Chatroom() {
     const {roomSlug} = useParams<{ roomSlug: string }>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string>("");
+    const [roomDetails, setRoomDetails] = useState<GetRoomDetailsResponse>(
+        {
+            name: ""
+        });
 
+    const fetchRoomDetails = useCallback(async () => {
+        setIsLoading(true);
+
+        try {
+            const res = await fetch(`https://localhost:7073/api/ChatRoom/${roomSlug}`);
+
+            if (res.status === 404) {
+                setError("Room Not Found");
+            } else {
+                const data = await res.json();
+                setRoomDetails(data)
+            }
+        } catch {
+            setError("Unable to load room.");
+        } finally {
+            setIsLoading(false);
+        }
+    }, [roomSlug]);
+
+    useEffect(() => {
+        void fetchRoomDetails();
+        }, [fetchRoomDetails]);
+    
     return (
 
         <>
-            <h1>{roomSlug}</h1>
+            {isLoading && (
+                <p>Loading room...</p>
+            )}
+
+            {error && (
+                <p>{error}</p>
+            )}
+
+            {!isLoading && (
+                <h1>{roomDetails.name}</h1>
+            )}
         </>
     )
 }
