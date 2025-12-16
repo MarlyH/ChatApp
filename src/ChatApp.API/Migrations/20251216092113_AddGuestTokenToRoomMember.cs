15ef38f83f8ffb4a988180167be31305cb021593
MarlyH <161419386+MarlyH@users.noreply.github.com>
2025-12-16 22:22:10 +1300

    Add GuestToken property to RoomMember model

---
+++ src/ChatApp.API/Migrations/20251216092113_AddGuestTokenToRoomMember.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGuestTokenToRoomMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GuestToken",
+                table: "RoomMembers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GuestToken",
+                table: "RoomMembers");
+        }
+    }
+}
--- src/ChatApp.API/Models/RoomMember.cs
+++ src/ChatApp.API/Models/RoomMember.cs
@@ -9,10 +9,16 @@ namespace ChatApp.Domain.Models
         public Guid RoomId { get; set; }
         [ForeignKey("RoomId")]
         public ChatRoom Room { get; set; } = null!;
-        public Guid? UserId { get; set; } // room member can be registered user or guest.
+
+        // registered user
+        public Guid? UserId { get; set; }
         [ForeignKey("UserId")]
         public AppUser? User { get; set; }
+
+        // guest user
+        public string? GuestToken { get; set; }
         public string? GuestName { get; set; }
+
         public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
     }
 }
